a86cb14834a1340d4de9a48c9d0db374f4ed6dd7
AxelBaron <axelbaron@outlook.fr>
2015-03-28 21:06:18 +0100

    PUTAIN CA MARCHE
    …

--- js/moduleRecherche.js
+++ js/moduleRecherche.js
@@ -13,16 +13,53 @@ app.config(['$routeProvider', function ($routeProvider) {
         });
 }]);
 
-app.controller('RechercheController', ['$scope', '$http', function ($scope, $http) {
-    $scope.nbLimit = 6;
-    $scope.count = 0;
-    $http.get('js/data.json').success(function (data) {
-        $scope.mesRealisations = data;
-    });
+app.controller('RechercheController', ['$scope', 'RechercheFactory', function ($scope, RechercheFactory) {
+    $scope.compteur = 6,
+    $scope.mesRealisations=RechercheFactory.getmesRealisations()
+        .then( 
+            function (data) { $scope.mesRealisations = data;},
+            function (msg) {
+                alert(msg); 
+            }
+        );
+    
+    $scope.nbmesRealisations = function () {
+        return ($scope.mesRealisations.length);
+    };
+    
+    $scope.count = function () {
+        return ($scope.mesRealisations.length);
+    };
+
 }]);
 
-app.controller("ViewRechercheController", ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.realisations = $scope.mesRealisations[$routeParams.nom];
+app.controller('ViewRechercheController', ['$scope', '$routeParams','RechercheFactory', function ($scope, $routeParams,RechercheFactory) {
+    
+     $scope.mesRealisations=RechercheFactory.getmesRealisations()
+        .then( 
+            function (data) { $scope.mesRealisations = data;$scope.realisations = $scope.mesRealisations[$routeParams.nom];},
+            function (msg) {
+                alert(msg); 
+            }
+        );
 }]);
 
-
+// PROMESSE EN CONSTRUCTION.
+app.factory('RechercheFactory', ['$http', '$q', function ($http, $q) {
+    var factory = {
+        mesRealisations: false,
+        getmesRealisations: function () {
+            var deferred = $q.defer();
+            $http.get('js/data.json')
+                .success(function (data, status) { 
+                    factory.mesRealisations = data; 
+                    deferred.resolve(factory.mesRealisations);
+                })
+                .error(function (data, status) {
+                    deferred.reject('Impossible de récupérer les données !'); 
+                })
+            return deferred.promise;
+        }
+    };
+    return factory;
+}]);
