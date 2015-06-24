858ce0df58dc8ca203080092e6229b2afa4341c8
AxelBaron <axelbaron@outlook.fr>
2015-06-24 14:23:10 +0200

    police + jquery

--- js/buttonActive.js
+++ js/buttonActive.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $('button').bind('click', function () {
+    $('.button').bind('click', function () {
         $('button').removeClass();
-        console.log('coucou');
         $(this).addClass('buttonActive');
+        $('#recherche > input').val('');
     });
 });
---
+++ js/min/buttonActive-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".button").bind("click",function(){$("button").removeClass(),$(this).addClass("buttonActive"),$("#recherche > input").val("")})});
