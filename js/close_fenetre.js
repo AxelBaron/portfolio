66e8ea4c3bbb5e9b96f6583f6730e4744efd857f
AxelBaron <axelbaron@outlook.fr>
2015-06-24 17:08:45 +0200

    pop up

--- js/close_fenetre.js
+++ js/close_fenetre.js
@@ -2,3 +2,11 @@ function close_fenetre(){
     document.getElementById('mask').style.display="none";
     document.getElementById('fenetre_mail').style.display="none";
 }
+
+function close_fmod_rea(){
+    var c = document.getElementById('contact');
+    c.style.position='block';
+    c.style.zIndex="0";
+    document.getElementById('mask_rea').style.display="none";
+    document.getElementById('fenetre_rea').style.display="none";
+}
