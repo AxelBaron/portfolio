c9f4ef4afa4748ce5b2acf7d94477a54e04d10f0
AxelBaron <axelbaron@outlook.fr>
2015-06-26 16:13:34 +0200

    FILTRE OK YEARRRR
    …

--- js/moduleRecherche.js
+++ js/moduleRecherche.js
@@ -14,15 +14,58 @@ app.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 app.controller('RechercheController', ['$scope', 'RechercheFactory', function ($scope, RechercheFactory) {
-    $scope.compteur = 6,
-    $scope.mesRealisations=RechercheFactory.getmesRealisations()
-        .then( 
-            function (data) { $scope.mesRealisations = data;},
-            function (msg) {
-                alert(msg); 
-            }
-        );
     
+    
+    $scope.compteur = 6,
+        
+    $scope.mesRealisations=RechercheFactory.getmesRealisations()
+    .then( 
+        function (data) { $scope.mesRealisations = data;},
+        function (msg) {
+            alert(msg); 
+        }
+    );
+    
+    $scope.filterRealisations=RechercheFactory.getmesRealisations()
+    .then( 
+        function (data) { $scope.filterRealisations = data;},
+        function (msg) {
+            alert(msg); 
+        }
+    );
+    
+    $scope.all = function () {
+        $scope.filterRealisations=$scope.mesRealisations;
+    };
+    
+    $scope.dev = function () {
+        $scope.filterRealisations =[];
+        for (var i = 0; i < $scope.mesRealisations.length; i++) {
+            if($scope.mesRealisations[i]['categorie'] == 'dev'){
+                $scope.filterRealisations.push($scope.mesRealisations[i]);
+            }
+        }
+    };
+    
+    $scope.illu = function () {
+        $scope.filterRealisations =[];
+        for (var i = 0; i < $scope.mesRealisations.length; i++) {
+            if($scope.mesRealisations[i]['categorie'] == 'illu'){
+                $scope.filterRealisations.push($scope.mesRealisations[i]);
+            }
+        }
+    };
+    
+    $scope.musique = function () {
+        $scope.filterRealisations =[];
+        for (var i = 0; i < $scope.mesRealisations.length; i++) {
+            if($scope.mesRealisations[i]['categorie'] == 'musique'){
+                $scope.filterRealisations.push($scope.mesRealisations[i]);
+            }
+        }
+    };
+        
+            
     $scope.nbmesRealisations = function () {
         return ($scope.mesRealisations.length);
     };
@@ -30,8 +73,13 @@ app.controller('RechercheController', ['$scope', 'RechercheFactory', function ($
     $scope.count = function () {
         return ($scope.mesRealisations.length);
     };
+    
 
 }]);
+
+function Test($scope) {
+  $scope.persons = [{type: 1, name: 'Caio'}, {type:2, name: 'Ary'}, {type:1, name: 'Camila'}];
+}
 
 app.controller('ViewRechercheController', ['$scope', '$routeParams', function ($scope, $routeParams) {
     $scope.realisation = $scope.mesRealisations[$routeParams.id];
